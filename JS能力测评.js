45551d798e5a64125556392365d8df2447dbc2d2
wangsiyuan233 <380191957@qq.com>
2018-06-29 17:53:37 -0700

    更新至34题

--- JS能力测评.js
+++ JS能力测评.js
@@ -371,7 +371,7 @@ function argsAsArray(fn, arr) {
 // 在JavaScript中，函数是一种对象，其上下文是可以变化的，对应的，函数内的this也是可以变化的，函数可以作为一个对象的方法，也可以同时作为另一个对象的方法，可以通过Function对象中的call或者apply方法来修改函数的上下文，函数中的this指针将被替换为call或者apply的第一个参数。将函数 fn 的执行上下文改为 obj 对象，只需要将obj作为call或者apply的第一个参数传入即可。
 
 function speak(fn, obj) {
-  return fn.apply(obj, obj);
+  return fn.apply(obj, obj); // call 也可以
  }
 
 // 题目二十一： 返回函数
@@ -394,7 +394,8 @@ function functionFunction(str) {
 // 2、运行 result 中第 i 个函数，即 result[i]()，结果与 fn(arr[i]) 相同
 
 // 简单的描述闭包：如果在函数func内部声明函数inner，然后在函数外部调用inner，这个过程即产生了一个闭包。
-// 题目要求的是返回一个函数数组，如果在循环中直接写result[i] = function(){return fn(arr[i]);}或者result.push(function(){return fn(arr[i]);})，最终的结果是不正确的，因为在每次迭代的时候，那样的语句后面的方法并没有执行，只是创建了一个函数体为“return fn(arr[i]);”的函数对象而已，当迭代停止时，i为最终迭代停止的值，在函数被调用时，i依旧为最终迭代停止的值，因此无法返回正确的结果。
+// 题目要求的是返回一个函数数组，如果在循环中直接写
+// result[i] = function(){return fn(arr[i]);} 或者 result.push(function(){return fn(arr[i]);})，最终的结果是不正确的，因为在每次迭代的时候，那样的语句后面的方法并没有执行，只是创建了一个函数体为“return fn(arr[i]);”的函数对象而已，当迭代停止时，i为最终迭代停止的值，在函数被调用时，i依旧为最终迭代停止的值，因此无法返回正确的结果。
 // 为了解决这个问题，需要声明一个匿名函数，并立即执行它。
 // function(num){return function(){return fn(arr[num]); }; }(i)，函数执行后，i立即传入并被内部函数访问到，因此就能得到正确的结果。闭包允许你引用存在于外部函数中的变量
 
@@ -408,39 +409,193 @@ function makeClosures(arr, fn){
   return result;
 }
 
-// 题目：
-// 描述：
+// 题目二十三： 二次封装函数
+// 描述：已知函数 fn 执行需要 3 个参数。请实现函数 partial，调用之后满足如下条件：
+// 1、返回一个函数 result，该函数接受一个参数
+// 2、执行 result(str3) ，返回的结果与 fn(str1, str2, str3) 一致
+
+// call和apply必须显式地调用str3，立即执行
+// bind不是立即执行，未传入str3时，并未执行，只是返回一个函数，等待参数传入
+// this用于上下文不确定的情况
+ 
+//方法1：call
+function partial(fn,str1,str2){
+  function result(str3){
+    return fn.call(this, str1,str2,str3);
+  }
+  return result;
+}
+ 
+//方法2：apply（这里只是为了对照）
+function partial(fn,str1,str2){
+  function result(str3){
+    return fn.apply(this, [str1,str2,str3]);
+  }
+  return result;
+}
+
+// 方法3：bind1
+// 这个bind会生成一个新函数（对象）, 它的str1, str2参数都定死了, str3未传入, 一旦传入就会执行
+function partial(fn, str1, str2){
+  return fn.bind(this, str1,str2);
+//或 return fn.bind(null, str1,str2);
+}
+
+// 方法3：bind2
+// bind同上, 多了一步, 把str3传入的过程写在另一个函数里面,
+// 而另一个函数也有str1, str2参数
+// 此法有种多次一举的感觉，但是表示出了后续的调用。
+function partial(fn,str1,str2){
+  function result(str3){
+    return fn.bind(this,str1,str2)(str3);
+  }
+  return result;
+}
+ 
+//方法4：匿名函数，默认this绑定global，与bind的第一个参数为this时效果一样。
+function partial(fn,str1,str2){
+  return function(str3){
+    return fn(str1,str2,str3);
+  }
+}
+ 
+//方法5： ES6的箭头函数， this指向undefined.
+const partial = (fn, str1, str2) => str3 => fn(str1, str2, str3);
 
 
-// 题目：
-// 描述：
+// 题目二十四：柯里化
+// 描述：已知 fn 为一个预定义函数，实现函数 curryIt，调用之后满足如下条件：
+// 1、返回一个函数 a，a 的 length 属性值为 1（即显式声明 a 接收一个参数）
+// 2、调用 a 之后，返回一个函数 b, b 的 length 属性值为 1
+// 3、调用 b 之后，返回一个函数 c, c 的 length 属性值为 1
+// 4、调用 c 之后，返回的结果与调用 fn 的返回值一致
+// 5、fn 的参数依次为函数 a, b, c 的调用参数
 
-// 题目：
-// 描述：
+function curryIt(fn){
+  var len = fn.length;
+      args = [];
+  var result = function(arg){
+    args.push(arg);
+    len --;
+    if(len <= 0){
+      return fn.apply(this, args);
+    }else{
+      return result;
+    }
+  }
+  return result;
+}
 
-// 题目：
-// 描述：
+// 题目二十五：或运算
+// 描述：返回参数 a 和 b 的逻辑或运算结果
+function or(a, b){return a||b;}
 
-// 题目：
-// 描述：
+// 题目二十六：且运算
+// 描述：返回参数 a 和 b 的逻辑且运算结果
+function and(a, b){return !!(a && b);}
 
-// 题目：
-// 描述：
-// 题目：
-// 描述：
+// 题目二十七：模块
+// 描述：完成函数 createModule，调用之后满足如下要求：
+// 1、返回一个对象
+// 2、对象的 greeting 属性值等于 str1， name 属性值等于 str2
+// 3、对象存在一个 sayIt 方法，该方法返回的字符串为 greeting属性值 + ', ' + name属性值
 
-// 题目：
-// 描述：
+function createModule(str1,str2){
+  var obj = {
+    greeting : str1,
+    name : str2,
+    sayIt : function(){
+      return this.greeting + ", " + this.name;
+    }
+  };
+  return obj;
+}
 
-// 题目：
-// 描述：
+// 题目二十八：二进制转换
+// 描述：获取数字 num 二进制形式第 bit 位的值。注意：
+// 1、bit 从 1 开始
+// 2、返回 0 或 1
+// 3、举例：2 的二进制为 10，第 1 位为 0，第 2 位为 1
 
-// 题目：
-// 描述：
+// 右移 （bit - 1） 位
+// & 1 意味着只输出左操作数的最后一位，而 bit 从 1 开始算。
+
+function valueAtBit(num, bit) {
+  return (num >> (bit -1)) & 1;
+}
 
 
-// 题目：
-// 描述：
+
+
+// 题目二十九：二进制转换
+// 描述：给定二进制字符串，将其换算成对应的十进制数字
+function base10(str){
+  return parseInt(str,2);
+}
+/**其它进制转十进制
+parseInt(str,2)
+parseInt(str,8)
+parseInt(str,16)
+**/
+
+// 题目三十：二进制转换
+// 描述：将给定数字转换成二进制字符串。如果字符串长度不足 8 位，则在前面补 0 到满8位。
+// 首先通过toString方法将num转为2进制数形式，然后判断其长度是否足够8位。如不足8位，则声明一个“0000000”字符串用于补0，因为目标的2进制数形式最少为一位，因此最多只需要7个0；通过slice方法对“0000000”进行截取，然后将其结果加在目标前面即可。
+
+function convertToBinary(num){
+// 转换为二进制格式
+  var s = num.toString(2);
+// 获得二进制数长度
+  var l = s.length;
+  if(l < 8){
+    // 声明一个字符串用于补满 0
+    var s1 = "0000000";
+    var s2 = s1.slice(0, 8 - l);
+    s = s2 + s;
+  }
+  return s;
+}
+// 题目三十一：乘法
+// 描述：求 a 和 b 相乘的值，a 和 b 可能是小数，需要注意结果的精度问题
+
+// 先将小数用10的n次方转化成整数，乘完的结果在除以之前乘了多少个10
+function multiply(a, b){
+  a = a.toString();
+  b = b.toString();
+
+  var aLen = a.substring(a.indexOf('.')+1).length;
+  var bLen = a.substring(a.indexOf('.')+1).length;
+
+  return (a * Math.pow(10,aLen)) * (b * Math.pow(10,bLen)) / Math.pow(10,aLen+bLen);
+}
+
+// 题目三十二：批量改变对象的属性
+// 描述：给定一个构造函数 constructor，请完成 alterObjects 方法，将 constructor 的所有实例的 greeting 属性指向给定的 greeting 变量。
+
+//这是原型链问题。访问一个对象的方法或者是属性，首先会在该对象中寻找，如果找到则返回，如果没找到，则在其原型链上面向上寻找，直至基原型，如还未找到，则返回undefined。将 constructor 的所有实例的 greeting 属性指向给定的 greeting 变量，只需要在constructor的原型上面添加greeting属性，并指定值。
+
+function alterObjects(constructor, greeting){
+  constructor.prototype.greeting = greeting;
+}
+
+// 题目三十三：属性遍历
+// 描述：找出对象 obj 不在原型链上的属性(注意这题测试例子的冒号后面也有一个空格~)
+// 1、返回数组，格式为 key: value
+// 2、结果数组不要求顺序
+
+// 可以使用for-in来遍历对象中的属性，hasOwnproperty方法能返回一个布尔值，指出一个对象是否具有指定名称的属性。此方法无法检查该对象的原型链中是否具有该属性，该属性必须为对象本身的属性。
+function iterate(obj){
+  var arr = [];
+//使用for-in遍历对象属性
+  for(var key in obj){
+//判断key是否为对象本身的属性
+    if(obj.hasOwnProperty(key)){
+//将属性和值按格式存入数组
+      arr.push(key + ': ' + obj[key]);
+    }
+  }
+  return arr;
+}
 
 // 题目：
 // 描述：
